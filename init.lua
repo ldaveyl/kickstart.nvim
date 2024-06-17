f414d75acd1be89bcf6a498b94d3659e66f89058
Lucas Davey <lucas.davey1@gmail.com>
2024-06-17 21:04:19 +0200

    update

--- init.lua
+++ init.lua
@@ -783,6 +783,9 @@ require('lazy').setup {
       { '<leader>4', '<cmd>Grapple select index=4<cr>', desc = 'Select fourth tag' },
       { '<leader>5', '<cmd>Grapple select index=5<cr>', desc = 'Select fifth tag' },
       { '<leader>6', '<cmd>Grapple select index=6<cr>', desc = 'Select sixth tag' },
+      { '<leader>7', '<cmd>Grapple select index=7<cr>', desc = 'Select seventh tag' },
+      { '<leader>8', '<cmd>Grapple select index=8<cr>', desc = 'Select eigth tag' },
+      { '<leader>9', '<cmd>Grapple select index=9<cr>', desc = 'Select ninth tag' },
 
       { '<leader>ss', '<cmd>Grapple toggle_scopes<cr>', desc = 'Show scopes' },
     },
